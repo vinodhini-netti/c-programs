a07051fdbd58f96086d36bd87a6eb05aad4e5ac4
vinodhini-netti <nettivinodhini@gmail.com>
2026-04-23 16:15:29 +0530

    Add files via upload

---
+++ consonant.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter the character\n");
+    scanf("%c",&ch);
+    int res = !((ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U')||(ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'));
+    printf("%c is the Consonant i.e is:%d",ch,res);
+    return 0;
+}
